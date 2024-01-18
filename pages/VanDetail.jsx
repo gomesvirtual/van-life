e791b3f4932ed3eb80b691df64fb03424160bf64
Sergio Gomes <gomesvirtual@gmail.com>
2024-01-18 08:55:14 -0300

    Update VanDetail

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 
 function VanDetail() {
-  const params = useParams();
+  const { id } = useParams();
   const [van, setVan] = useState(null);
 
   useEffect(() => {
-    fetch(`/api/vans/${params.id}`)
+    fetch(`/api/vans/${id}`)
       .then(res => res.json())
       .then(data => setVan(data.vans));
-  }, [params.id])  
+  }, [id])  
 
   return ( 
     <div className="van-detail-container">
